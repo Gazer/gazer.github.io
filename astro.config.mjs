a740445df0c7be801e999ef66c55e790592cf5bc
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2023-12-08 19:30:38 +0100

    fix gists

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gazer.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+    mdx(),
+    sitemap(),
+  ],
 });
